f44c7d089148f48e98385610e81689fe67ca8bcd
Grad Laurentiu <gradlaur@yahoo.com>
2024-05-29 23:47:02 +0300

    Update vec3.cpp

--- EGC_Lab8/EGC_CBclip/vec3.cpp
+++ EGC_Lab8/EGC_CBclip/vec3.cpp
@@ -46,6 +46,6 @@ namespace egc {
         return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
     };
     vec3 crossProduct(const vec3& v1, const vec3& v2) {
-        return vec3(v1.y * v2.z - v1.z * v2.y, v1.x * v2.z - v1.z * v2.x, v1.x * v2.y - v1.y * v2.z);
+        return vec3(v1.y * v2.z - v1.z * v2.y, - v1.x * v2.z + v1.z * v2.x, v1.x * v2.y - v1.y * v2.z);
     }
 };
